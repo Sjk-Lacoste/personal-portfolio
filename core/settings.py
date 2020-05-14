8ce49d699d93199eed148e0899f59f393e788401
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-05-14 15:52:11 +0200

    Settings for django and react on heroku

--- core/settings.py
+++ core/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
     'skills',
     'projects',
     'clients',
-    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -141,11 +140,11 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 # we whitelist localhost:3000 because that's where frontend will be served
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8000',
-    'http://localhost:3000',
-)
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
+#     'http://localhost:8000',
+#     'http://localhost:3000',
+# )
 
 
 # Internationalization
