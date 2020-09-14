df88a7c4c4f9e517f8c0adeee1c92b44fdd5684b
TshepoMohlatlole <tshepo@turati.co.za>
2020-09-14 14:45:22 +0200

    Added migrations

--- core/settings.py
+++ core/settings.py
@@ -94,12 +94,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'portfolio',
-        'USER': 'sjk_lacoste',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
